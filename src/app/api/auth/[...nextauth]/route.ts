7e9a505cbb8ea811b701fa4dd631eca6546f04bd
rohling <adairrohling@gmail.com>
2023-11-13 18:12:58 -0300

    fix:auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,18 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
-
-const authOptions: AuthOptions = {
+import GoogleProvider from 'next-auth/providers/google'
+// Here we removed the export
+const authOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
-  ],
-};
-
-export const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ]
+}
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST, authOptions }
